96c0badc9b7eb4fb3ab5ff98349d38b9b753df36
Jared Thach <jared.thach@chargie.com>
2024-06-01 16:46:54 -0700

    connect to database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
 app.get('/', (req, res) => {
     res.send("Hello from Node API");
+});
+
+// First, connect to database
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@backenddb.zkb6dyb.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`)
+  .then(() => {
+    console.log('Database connected.');
+
+    // Second, connect to server
+    app.listen(process.env.EXPRESS_SERVER_PORT, () => {
+        console.log(`Server is running on port ${process.env.EXPRESS_SERVER_PORT}.`);
+    });
+    
+})
+.catch(() => {
+    console.log('Database connection failed.');
 });
