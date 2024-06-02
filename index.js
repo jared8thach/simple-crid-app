26c6882ecda6a906e09b45308551b894726cebcb
Jared Thach <jared.thach@chargie.com>
2024-06-01 17:18:05 -0700

    adding Product model to post endpoint

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+
 const app = express();
 
+// using JSON formatting
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send("Hello from Node API");
+    res.send('Hello from Node API');
+});
+
+// by default, Express does not allow JSON format in request
+// therefore, we must establish a middleware
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 // First, connect to database
---
+++ models/product.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// schema for a Product object
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter product name"],
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        
+        image: {
+            type: String,
+            required: false
+        },
+    },
+    {
+        // this will log when the entry was created at and updated at
+        timestamps: true
+    },
+);
+
+// creating Product model using Product schema
+const Product = mongoose.model('Product', ProductSchema);
+
+// exporting Product
+module.exports = Product;
